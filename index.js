7e4a694b8dd738e9d1724804617f254187feaa3f
Landon <landoncramer1@gmail.com>
2023-09-17 16:08:14 -0700

    filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(driverNameArr, name) {
+    const result = driverNameArr.filter((driver) => {
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+    return result;
+}
+
+function fuzzyMatch(driverArr, name) {
+      return driverArr.filter((driver) => {
+            return driver.toLowerCase().startsWith(name.toLowerCase());
+        })
+    }
+
+   
+    function matchName(driverObjArr, name) {
+           return driverObjArr.filter(driver => {
+           return driver.name === name
+           })
+    }
